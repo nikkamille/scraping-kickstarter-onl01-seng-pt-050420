6693773721f2b4cabe468b4f94a9dc251ab016fa
Kamille Longalong <k.pagibigan@gmail.com>
2020-06-11 05:32:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,20 +2,28 @@
 require 'nokogiri'
 require 'pry'
 
-def create_project_hash
-  
-  html = File.read('fixtures/kickstarter.html')
-  
-  kickstarter = Nokogiri::HTML(html)
-  
-  binding.pry
-  
   # projects: kickstarter.css("li.project.grid_4").first
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
   # location: project.css("span.location-name").text
   # percent_funded: project.css("li.first.funded strong").text.gsub("%", "").to_i
+
+def create_project_hash
+  
+  html = File.read('fixtures/kickstarter.html')
+  
+  kickstarter = Nokogiri::HTML(html)
+  
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+  
+  projects
+  
+ 
 end
 
-create_project_hash
